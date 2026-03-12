b0cdf642156574c321ca0c9a5d3b477884fd486d
wisnu-10 <wisnubintoro1997@gmail.com>
2026-03-12 21:04:33 +0700

    added error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
+
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err);
+  const statusCode = err.expose === true ? err.statusCode : 500;
+  const message = err.expose === true ? err.message : 'Something went wrong';
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    data: null,
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
