338df9c8c14028ec18e2df23178cd379bc0791e0
wisnu-10 <wisnubintoro1997@gmail.com>
2026-04-07 17:49:51 +0700

    fix on update data employee

--- src/modules/super-admin/validators/update-employee.validator.ts
+++ src/modules/super-admin/validators/update-employee.validator.ts
@@ -43,4 +43,13 @@ export const updateEmployeeValidator = [
     .optional()
     .isNumeric()
     .withMessage("Bank account number must contain only numbers"),
+
+  body("password")
+    .optional()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/\d/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter"),
 ];
